1266ae93977973db1eae2125633dd28901a09e53
Jesse <jn2934@nyu.edu>
2025-11-30 19:34:12 -0500

    Added a template.yaml

--- index_photos/LF1.py
+++ index_photos/LF1.py
@@ -9,7 +9,7 @@ from botocore.awsrequest import AWSRequest
 from botocore.auth import SigV4Auth
 import boto3
 
-#SUCCESSS IF THIS COMMENT IS HERE 1
+#SUCCESSS IF THIS COMMENT IS HERE 3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
